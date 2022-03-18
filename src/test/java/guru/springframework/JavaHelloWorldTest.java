5e27f6807404c3ebe268a854c95d80c5c0b94b67
Alex <azakotyansky@gmail.com>
2022-03-18 14:35:38 -0400

    Added <activation>              <activeByDefault>true</activeByDefault>          </activation>

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -2,12 +2,15 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Set in pom.xml
+ */
 class JavaHelloWorldTest {
 
     @Test
     void getHello() {
 
         System.out.println("#####################");
-        System.out.println(System.getenv("TEST_HOST"));
+        System.out.println("Set TEST_HOST property in pom.xml: " + System.getenv("TEST_HOST"));
     }
 }
