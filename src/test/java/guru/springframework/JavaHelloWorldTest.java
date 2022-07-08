5ce5b7e05a38f97df6ad38ef3cba00721c5c89aa
Alex <azakotyansky@gmail.com>
2022-07-08 16:29:38 -0400

    Added <activation>              <activeByDefault>true</activeByDefault>          </activation>

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -2,7 +2,10 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JavaHelloWorldTest {
 
@@ -11,5 +14,8 @@ class JavaHelloWorldTest {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
 
         assertEquals("Hello World", javaHelloWorld.getHello());
+
+        Supplier<String> messageSupplier  = () -> "Calculator.add(2, 2) test failed";
+        assertTrue(1 < 2, messageSupplier);
     }
 }
