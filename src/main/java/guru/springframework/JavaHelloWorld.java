41538063856dec7a4b6d1244b8947eb4b2307999
Alex <azakotyansky@gmail.com>
2022-07-22 19:53:35 -0400

    Added <activation>              <activeByDefault>true</activeByDefault>          </activation>

--- src/main/java/guru/springframework/JavaHelloWorld.java
+++ src/main/java/guru/springframework/JavaHelloWorld.java
@@ -1,5 +1,6 @@
 package guru.springframework;
 
+
 /**
  * Created by jt on 2018-12-09.
  */
@@ -7,6 +8,14 @@ public class JavaHelloWorld {
 
     public String getHello(){
         return "Hello World";
+    }
+
+
+    public String findBug(String s) throws Exception {
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(String.format(".%s", s.charAt(i)));
+        }
+        return s;
     }
 
 }
